183b02365759c3073db7be226e44b3e9e871af15
manu <manu>
2015-02-26 23:13:57 +0100

    add packages add kadira monitoring performance tool

--- server/startup.js
+++ server/startup.js
@@ -3,27 +3,33 @@
  */
 Meteor.startup(function () {
     // code to run on server at startup
-
-    function initializeFakeRooms() {
-       
-       for(var i = 0; i < 50; i++){
-          Rooms.insert({title: faker.name.findName(), creationDate: new Date()});
-       }
-       /* Rooms.insert({title: 'Hello world', creationDate: new Date()});
-        Rooms.insert({title: 'Hello world', creationDate: new Date()});
-        Rooms.insert({title: 'Hello world', creationDate: new Date()});
-        Rooms.insert({title: 'Hello world', creationDate: new Date()});
-        Rooms.insert({title: 'Hello world', creationDate: new Date()});
-        Rooms.insert({title: 'Hello world', creationDate: new Date()});
-        Rooms.insert({title: 'Hello world', creationDate: new Date()});
-        Rooms.insert({title: 'Hello world', creationDate: new Date()});
-        Rooms.insert({title: 'Hello world', creationDate: new Date()});
-        Rooms.insert({title: 'Hello world', creationDate: new Date()});
-        Rooms.insert({title: 'Hello world', creationDate: new Date()});
-        Rooms.insert({title: 'Hello world', creationDate: new Date()});
-        Rooms.insert({title: 'Hello world', creationDate: new Date()});*/
+    function initializeApplication() {
+        Kadira.connect('hzKYxH6FyL4g9SM5v', '531de270-f860-4f5f-aeb2-15e785cb0731');
     }
 
+
+    function initializeFakeRooms() {
+
+        for(var i = 0; i < 50; i++){
+            Rooms.insert({title: faker.name.findName(), creationDate: new Date()});
+        }
+        /* Rooms.insert({title: 'Hello world', creationDate: new Date()});
+         Rooms.insert({title: 'Hello world', creationDate: new Date()});
+         Rooms.insert({title: 'Hello world', creationDate: new Date()});
+         Rooms.insert({title: 'Hello world', creationDate: new Date()});
+         Rooms.insert({title: 'Hello world', creationDate: new Date()});
+         Rooms.insert({title: 'Hello world', creationDate: new Date()});
+         Rooms.insert({title: 'Hello world', creationDate: new Date()});
+         Rooms.insert({title: 'Hello world', creationDate: new Date()});
+         Rooms.insert({title: 'Hello world', creationDate: new Date()});
+         Rooms.insert({title: 'Hello world', creationDate: new Date()});
+         Rooms.insert({title: 'Hello world', creationDate: new Date()});
+         Rooms.insert({title: 'Hello world', creationDate: new Date()});
+         Rooms.insert({title: 'Hello world', creationDate: new Date()});*/
+    }
+
+    initializeApplication();
+    
     if(Rooms.find().count() === 0){
         initializeFakeRooms();
     }
