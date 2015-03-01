8956358dc3f45769c0b88d14504443d234f86263
manu <manu>
2015-03-01 20:08:25 +0100

    add rooms

--- client/components/rooms/rooms.js
+++ client/components/rooms/rooms.js
@@ -1,3 +1,10 @@
 /**
  * Created by emmanuelernest on 01/03/15.
  */
+
+Template.rooms.helpers({
+    latestRooms : function(){
+        var results = Rooms.find({},{sort : {date: -1}, limit : 20});
+        return results;
+    }
+});
